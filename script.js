6a42a57faa3fe89c94f7119468e7238284a73fbc
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-10-06 20:54:45 -0400

    Created DeckOfCards object constructor. Initial version of newDeck() function added

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-var baseURL = "https://deckofcardsapi.com/api/deck"
-
 // Deck of Cards Object to be use the API.
-function DeckOfCards() {
+function DeckOfCards(number = 1) {
+    this.baseURL = "https://deckofcardsapi.com/api/deck",
+    this.id = "",
+    this.shuffled = "",
+    this.remaining = "",
 
+    this.shuffle = function() {
+
+    },
+
+    this.draw = function() {
+
+    },
+
+    this.newDeck = function(addJokers = false) {
+        var url = this.baseURL + "new/";
+        if (addJokers) {
+            const fetchOptions = {
+                method: "GET",
+                headers: {
+                    "jokers_enabled": "true"
+                },
+            }
+        }
+
+        fetch(url, fetchOptions)
+            .then((response) => response.json())
+            .then((result) => console.log())
+
+    },
+
+    this.returnToDeck = function() {
+
+    }
 };
